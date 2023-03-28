22b9cbcf09e59a03b9133eba451aa656bda6d5e1
karim-aboelazm <20912017100480@fci.zu.edu.eg>
2023-03-28 12:06:05 +0200

    Adding Iframe

--- islamic/views.py
+++ islamic/views.py
@@ -4,3 +4,11 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'index.html'
+    
+class ProphetsStoriesView(TemplateView):
+    template_name = "prophets_stories.html"
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context[""] = 
+        return context
+    
